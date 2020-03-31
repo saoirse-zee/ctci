f6ded235641d63e60b58a4b2db3ee2ecb4a93101
Emma Smith <saoirse-zee@users.noreply.github.com>
2020-03-31 09:18:58 -0400

    2.1 Improve `removeDupes`

--- 2-linked-list/2.1-remove-dupes/remove-dupes.js
+++ 2-linked-list/2.1-remove-dupes/remove-dupes.js
@@ -7,27 +7,18 @@ const LinkedList = require("../linked-list");
  * @param {LinkedList} list
  */
 function removeDupes(list) {
+  const seen = new Set();
   let current = list.head;
   let prev = null;
-  const valuesSet = new Set();
 
-  while (current.next !== null) {
-    if (valuesSet.has(current.value)) {
-      // Remove the current node by updating the "next pointer" of the previous node. Note that `prev` itself stays where it is!
+  while (current !== null) {
+    if (seen.has(current.value)) {
       prev.next = current.next;
     } else {
-      valuesSet.add(current.value);
-      // Iterate `prev`
+      seen.add(current.value);
       prev = current;
     }
-    // Iterate `current`
     current = current.next;
-  }
-
-  // Check the last node
-  if (valuesSet.has(current.value)) {
-    // Remove the last node
-    prev.next = null;
   }
 }
 
