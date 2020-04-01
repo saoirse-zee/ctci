d8800dbb1edbf54e69604c2ffe5d01a8803532f7
Emma Smith <saoirse-zee@users.noreply.github.com>
2020-04-01 09:22:54 -0400

    1.3

---
+++ 1-arrays-and-strings/1.3-urlify.js
@@ -0,0 +1,14 @@
+/**
+ * Replaces spaces with url-friendly characters.
+ *
+ * @param {string} s - A string with spaces
+ */
+function urlify(s) {
+  return s
+    .trim()
+    .split(" ")
+    .filter(substring => substring !== "")
+    .join("%20");
+}
+
+module.exports = urlify;
