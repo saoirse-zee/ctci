95801487f9c183cc74dba85df12531c17bb98da3
Emma Smith <saoirse-zee@users.noreply.github.com>
2020-04-06 09:22:37 -0400

    1.4

---
+++ 1-arrays-and-strings/1.4-is-palindrome-permutation.js
@@ -0,0 +1,19 @@
+function isPal(s) {
+  const sanitized = s.toUpperCase().split(" ").join("");
+  const freq = new Map();
+  for (let i = 0; i < sanitized.length; i++) {
+    const char = sanitized.charAt(i);
+    const prevFreq = freq.get(char) || 0;
+    freq.set(char, prevFreq + 1);
+  }
+
+  let oddCount = 0;
+  for (let char of freq) {
+    if (char[1] % 2 !== 0) {
+      oddCount++;
+    }
+  }
+  return oddCount < 2;
+}
+
+module.exports = isPal;
---
+++ 1-arrays-and-strings/1.4-is-palindrome-permutation.test.js
@@ -0,0 +1,20 @@
+const test = require("ava");
+const isPalindromePermutation = require("./1.4-is-palindrome-permutation");
+
+test("correctly identifies an all-unique string", (t) => {
+  const input = "Tact Coa";
+  const result = isPalindromePermutation(input);
+  t.true(result);
+});
+
+test("correctly identifies a string with repeating characters", (t) => {
+  const input = "boo boo";
+  const result = isPalindromePermutation(input);
+  t.true(result);
+});
+
+test("correctly identifies a non-palindrome", (t) => {
+  const input = "abcde";
+  const result = isPalindromePermutation(input);
+  t.false(result);
+});
